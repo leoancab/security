b136471feb0f97bb5361181763a036ce5647aedc
leoancab <leoancab@espol.edu.ec>
2024-07-17 10:34:39 -0500

    guía 18 stop 3

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const sequelize = require('../models/index.js').sequelize;
+var initModels = require("../models/init-models");
+var models = initModels(sequelize);
+
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.render('crud');
+router.get('/', async function (req, res, next) {
+
+  /* 3. Uso del método findAll */
+  let usersCollection = await models.users.findAll({})
+
+  /* 4. Paso de parámetros a la vista */
+  res.render('crud', { title: 'CRUD with users', usersArray: usersCollection });
+
 });
 
 module.exports = router;
