f0fe99321f7cbc55b5fca008acc290fccd44eea4
leoancab <leoancab@espol.edu.ec>
2024-07-22 10:55:16 -0500

    guía 19 stop 3

--- routes/users.js
+++ routes/users.js
@@ -13,10 +13,10 @@ router.get('/', async function (req, res, next) {
 
   /* 3. Uso del método findAll */
   let usersCollection = await models.users.findAll({})
+  let rolesCollection = await models.roles.findAll({})
 
   /* 4. Paso de parámetros a la vista */
-  res.render('crud', { title: 'CRUD with users', usersArray: usersCollection });
-
+  res.render('crud', { title: 'CRUD of users', usersArray: usersCollection, rolesArray: rolesCollection });
 });
 
 /* POST user. */
